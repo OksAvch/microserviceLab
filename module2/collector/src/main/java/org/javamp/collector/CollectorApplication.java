51df7fe467adeeee0993ad975dedcc5bf98a2824
oksAvch <avchynnikova.oksana@gmail.com>
2023-12-10 22:14:53 +0100

    [Module 2] collector and recipient are connected

--- module2/collector/src/main/java/org/javamp/collector/CollectorApplication.java
+++ module2/collector/src/main/java/org/javamp/collector/CollectorApplication.java
@@ -2,8 +2,13 @@ package org.javamp.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
+@ConfigurationProperties
+@PropertySource("classpath:application.yml")
 public class CollectorApplication {
 
     public static void main(String[] args) {
--- module2/collector/src/main/java/org/javamp/collector/integration/RecipientIntegrationImpl.java
+++ module2/collector/src/main/java/org/javamp/collector/integration/RecipientIntegrationImpl.java
@@ -1,6 +1,7 @@
 package org.javamp.collector.integration;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,16 +11,17 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class RecipientIntegrationImpl implements RecipientIntegration {
-    public static final String RECIPIENT_URL = "http://localhost:8091/messages";
     public static final String SPLITERATOR = ", ";
 
+    @Value("${integration.recipient.url}")
+    private String recipientUrl;
     private final RestTemplate restTemplate;
 
     @Override
     public List<String> pullMessages() {
-        String response = restTemplate.getForObject(RECIPIENT_URL, String.class);
+        String response = restTemplate.getForObject(recipientUrl, String.class);
 
         return Optional.ofNullable(response)
                 .map(r -> Arrays.asList(r.split(SPLITERATOR)))
